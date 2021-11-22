7b5ab2d6f382bd5de581d87fcb4ee78485cc9884
edoinjp <edoinjp@gmail.com>
2021-11-22 03:52:10 -0800

    eduard gurchiani
    slice / find functions

---
+++ script.js
@@ -0,0 +1,18 @@
+const array1 = [28, 15, 12, 33, 11, 19, 199];
+
+const found = array1.find((element) => element < 19);
+
+console.log(found);
+
+// am "kodist" shegvidzlia vipovot pirveli elementi cxrilshi romelic metia chvens mier mopiqrebul ricxvze (19)
+// zustad ver vxvdebi rogor shemudzlia gamoviyeno radgan marto ert elements poulobs da ara yvelas romelic metia an naklebia (19ze)
+
+const cars = ["audi", "mercedes", "ferrari", "lamborgini", "fiat"];
+
+console.log(cars.slice(2));
+console.log(cars.slice(4));
+
+// am "kodit" shegvidzlia cxrilis satavedan gavaqrot imdeni elementi ramdenic gvchirdeba
+// aqac ver vxvdebi zustad ratom sheidzleba damchires magalitad teleponis ekranze morgebis dros shegvidzlia gavaqrot tu ar etebva ekranze (albat)
+
+// eduard gurchiani
